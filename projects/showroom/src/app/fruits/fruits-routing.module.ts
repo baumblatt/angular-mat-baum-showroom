77b063697af13be7dc4d6a9b5ad80a866fbc26cb
Bernardo Baumblatt <baumblatt@gmail.com>
2020-09-22 09:14:53 -0300

    angular-mat-baum@1.0.0-beta.17

--- projects/showroom/src/app/fruits/fruits-routing.module.ts
+++ projects/showroom/src/app/fruits/fruits-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntryContainer } from './containers/entry/entry.container';
+import { JuicesContainer } from './containers/juices/juices.container';
+import { CandiesContainer } from './containers/candies/candies.container';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'entry'},
+  {path: 'entry', component: EntryContainer},
+  {path: 'juices', component: JuicesContainer},
+  {path: 'candies', component: CandiesContainer},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
